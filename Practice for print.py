cacd1749566dab99a5889ab1893d335fd450c2e9
Marwa Akeel <marwaakeel2020@gmail.com>
2024-10-14 09:23:46 +0400

    Add files via upload

---
+++ Practice for print.py
@@ -0,0 +1,24 @@
+print("Hello, World!") #Basic strring output
+print(5 + 3) #Simple math inside print
+print("Python","is","fun!") #Multiple arguments in print
+print("She said,\"Hello!\"") #Print with escape character
+print("First line\nSecond line") #Newline character
+print("Item 1\tItem 2\tItem 3") #Tab character
+print("Then result is", 42) #Print multiple datatypes together
+print(12345) #Print a single number
+print(3.14159) #Print a decimal number
+print("Dollar sign:$") #Print special character
+print("apple","banana","cherry",sep=",") #Using the sep parameter
+print("Hello,",end="")
+print("World!") #Using the end parameter
+print("python!"*3) #Print a rrepeated string
+print("\u2764") #Prints a heart symbol
+print() #Print empty line
+print("Hello"+"World!") #Concattenating strings inside print
+print(True)
+print(False) #Print boolean values
+print(1.2e3) #Prints 1200.0
+print("""This is a multiline
+print statement.""" ) #Print multipline text using triple quotes
+print("This is a backslash:\\") #Print using escaped blackslash
+
